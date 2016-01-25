8cf1109f30fd56e8266db492660836baaef1b684
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-01-25 15:54:59 +0300

    printer.js: progect finished, depends on lp and imagemagic

--- printer.js
+++ printer.js
@@ -5,6 +5,7 @@
 
 	const path = require('path');
 	const fs = require('fs');
+	const child_process = require('child_process');
 
 	const printers = require('optManager').getopt.create([]).parseSystem().argv;
 
@@ -12,7 +13,9 @@
 	const LOGS = path.join(PWD, 'logs');
 	const ERROR_LOG = path.join(LOGS, 'error.log');
 
-	console.log(printers);
+	function parseErr (err) {
+		return [[err.syscall, err.path].join(' '), err.code].join(' - ');
+	}
 
 	function errorLog (str) {
 		str = [new Date(), str].join(': ');
@@ -21,7 +24,7 @@
 				err.log = str;
 				throw err;
 			}
-			fs.appendFile(fd, [str, '\n'].join(''), (err) => {
+			fs.appendFile(fd, str + '\n', (err) => {
 				if (err) {
 					err.log = str;
 					throw err;
@@ -36,56 +39,164 @@
 		});
 	}
 
+	function addLog (file, str) {
+		str = [new Date(), str].join(': ');
+		fs.open(path.join(LOGS, file), 'a', (err, fd) => {
+			if (err) {
+				errorLog(parseErr(err));
+			} else {
+				fs.appendFile(fd, str + '\n', (err) => {
+					if (err) {
+						errorLog(parseErr(err));
+					} else {
+						fs.close(fd, (err) => {
+							if (err)
+								errorLog(parseErr(err));
+						});
+					}
+				});
+			}
+		});
+	}
+
 	function createLogFile (name, callback) {
 		const logFile = [name, 'log'].join('.');
 		const fullPath = path.join(LOGS, logFile);
 		fs.open(fullPath, 'w', (err, fd) => {
-			if (err)
-				return errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-			fs.close(fd, (err) => {
-				if (err)
-					return errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-				callback(name, fullPath);
-			});
+			if (err) {
+				errorLog(parseErr(err));
+			} else {
+				fs.close(fd, (err) => {
+					if (err)
+						errorLog(parseErr(err));
+					else
+						callback(name, fullPath);
+				});
+			}
 		});
 	}
 
-	function print (printer) {}
+
+
+	function print (printer) {
+		function _printFile (file) {
+			const spawn = child_process.spawn;
+
+			var identify = spawn('identify', [file]),
+			    info = '';
+
+			identify.stderr.on('data', (data) => {errorLog(data.toString('utf8'))});
+			identify.stdout.on('data', (data) => {info += data.toString('utf8')});
+			identify.on('close', (code) => {
+				if (code) {
+					errorLog('image magic closed whith code ' + code);
+				} else {
+					info = /\b\d+x\d+\b/.exec(info)[0];
+					if (!info) {
+						errorLog('Can not get file resolution: ' + file);
+					} else {
+						info = info.split('x');
+						var lpoptions = [file, '-d', printer];
+
+						if (info[0] > info[1])
+							lpoptions.push('-o landscape');
+						var lp = spawn('lp', lpoptions);
+
+						lp.stderr.on('data', (data) => {errorLog(printer + ": " + data.toString('utf8'))});
+						lp.on('close', (code) => {
+							if (code) {
+								errorLog(printer + ': Print error: ' + code);
+							} else {
+								addLog([printer, 'log'].join('.'), 'Printing file: ' + file);
+							}
+						})
+					}
+					
+				}
+			});
+		}
+
+		var files = [];
+		function _createFileList (pathes) {
+			var p;
+			while (!(p = pathes.shift()) && pathes.length);
+			if (p) {
+				p = path.resolve(p);
+				
+				fs.stat(p, (err, stats) => {
+					if (err) {
+						errorLog(parseErr(err));
+					} else {
+						if (stats.isDirectory()) {
+							fs.readdir(p, (err, files) => {
+								if(err) {
+									errorLog(parseErr(err));
+								} else {
+									for (var i = 0, l = files.length; i < l; ++i)
+										files[i] = path.join(p, files[i]);
+									_createFileList(pathes.concat(files));
+								}
+							});
+						} else {
+							if(stats.isFile() && {gif: 1, png: 1, jpeg: 1, jpg: 1}[path.extname(p).substr(1)]);
+								files.push(p);
+							_createFileList(pathes);
+						}
+					}
+				});
+			} else {
+				_printFile(files[Math.floor(files.length * Math.random())]);
+			}
+		}
+
+		fs.readFile(path.join(PWD, 'pathList.path'), {encoding: 'utf8'}, (err, data) => {
+			if (err) {
+				errorLog(parseErr(err));
+			} else {
+				if (!data)
+					errorLog(path.join(PWD, 'pathList.path') + ' is empty.');
+				else
+					_createFileList(data.split('\n'));
+			}
+		});
+	}
 
 	function checkLastDate (name, file) {
 		fs.open(file, 'r', (err, fd) => {
 			if(err) {
-				if (err.code === 'ENOENT')
-					return createLogFile(name, readLastDate);
-				errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-				return print(name);
+				if (err.code === 'ENOENT') {
+					createLogFile(name, checkLastDate);
+				} else {
+					errorLog(parseErr(err));
+					print(name);
+				}
+			} else {
+				var line = '';
+				const SIZE = 10;
+				function _findLastDate (pos) {
+					fs.read(fd, new Buffer(SIZE), 0, SIZE, pos, (err, bytesRead, buffer) => {
+						if (err) {
+							errorLog(parseErr(err));
+							print(name);
+						} else {
+								var str = buffer.toString('utf-8', 0, bytesRead);
+							if (str.search('\n') < 0)
+								line += str;
+							else 
+								line = str.substr(str.lastIndexOf('\n') + 1) || line;
+							if (bytesRead === SIZE) {
+								_findLastDate (pos + bytesRead);
+							} else {
+								line = line.split(':')[0];
+								
+								if ((new Date().getTime() - new Date(line || 0).getTime()) / (1000 * 60 * 60 * 23) >= 7)
+									print(name);
+							}
+						}
+					});
+				}
+				_findLastDate(0);
 			}
-
-			var line = '',
-			    size = 10;
-			function _findLastDate (pos) {
-				fs.read(fd, new Buffer(size), 0, size, pos, (err, bytesRead, buffer) => {
-					if (err) {
-						errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-						return print(name);
-					}
-
-					var str = buffer.toString('utf-8', 0, bytesRead);
-					if (str.search('\n') < 0)
-						line += str;
-					else 
-						line = str.substr(str.lastIndexOf('\n') + 1) || line;
-					if (bytesRead === size) {
-						_findLastDate (pos + bytesRead);
-					} else {
-						line = line.split(':')[0];
-						
-						if (new Date().getTime() - new Date(line || 0).getTime() / (1000 * 60 * 60 * 23) >= 7)
-							print();
-					}
-				});
-			}
-			_findLastDate(0);
 		});
 	}
 
@@ -94,18 +205,19 @@
 			if (err.code === 'ENOENT')
 				fs.mkdir(LOGS, (err) => {
 					if (err)
-						return errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-					for (var i = 0, l = printers.length; i < l; ++i)
-						createLogFile(printers[i], readLastDate);
+						errorLog(parseErr(err));
+					else
+						for (var i = 0, l = printers.length; i < l; ++i)
+							createLogFile(printers[i], checkLastDate);
 				});
 			else
-				errorLog([[err.syscall, err.path].join(' '), err.code].join(' - '));
-		}
-
-		for (var i = 0, l = printers.length; i < l; ++i) {
-			var printer = printers[i],
-			    file = path.join(LOGS, [printer, 'log'].join('.'));
-			readLastDate(printer, file);
+				errorLog(parseErr(err));
+		} else {
+			for (var i = 0, l = printers.length; i < l; ++i) {
+				var printer = printers[i],
+				    file = path.join(LOGS, [printer, 'log'].join('.'));
+				checkLastDate(printer, file);
+			}
 		}
 
 	});
